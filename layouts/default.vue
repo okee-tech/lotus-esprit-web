505b6fdcf9f7e86405fdd976996cd287892957ef
Demid Kaidalov <demid.kaidalov@gmail.com>
2024-12-04 18:21:22 +0200

    Refactor app layout and add default layout component; update dependencies

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-screen min-w-screen">
+    <!-- Sticky header -->
+    <header class="w-full bg-base-100 shadow-lg">
+      <nav class="navbar container mx-auto px-4">
+        <div class="flex-1">
+          <div>DeLorean</div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Main content -->
+    <main class="h-full container mx-auto px-4 py-6 overflow-auto">
+      <slot />
+    </main>
+  </div>
+</template>
