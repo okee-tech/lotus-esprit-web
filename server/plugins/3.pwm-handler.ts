a712c2ffc828340f299ee556ef0088f8f27ad052
Demid Kaidalov <demid.kaidalov@gmail.com>
2024-12-05 02:55:38 +0200

    Add PWM handling with GPIO integration; set mode and update PWM value

--- server/plugins/3.pwm-handler.ts
+++ server/plugins/3.pwm-handler.ts
@@ -1,9 +1,13 @@
 import { SharedState } from "./2.socket-shared-state";
+import { Gpio, Mode } from "@okee-tech/rppal";
 
-// const gpio = new Gpio();
-// const p2 = gpio.get(2);
+const gpio = new Gpio();
+const p2 = gpio.get(2);
+p2.mode = Mode.Output;
+p2.clearPwm();
 
 async function onPwmUpdate(newValue: number[]) {
+  p2.setPwm(newValue[0], 0.5);
   console.log("Pwm update FROM SERVER: ", newValue);
 }
 
